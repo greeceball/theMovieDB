99c6db801b944945388d86c2bb6ec70932b1c8e7
Colby Harris <griff33@gmail.com>
2020-03-27 09:26:05 -0600

    First Commit

---
+++ theMovieDBObjC/Model/CHMovie.h
@@ -0,0 +1,17 @@
+//
+//  CHMovie.h
+//  theMovieDBObjC
+//
+//  Created by Colby Harris on 3/27/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHMovie : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ theMovieDBObjC/Model/CHMovie.m
@@ -0,0 +1,13 @@
+//
+//  CHMovie.m
+//  theMovieDBObjC
+//
+//  Created by Colby Harris on 3/27/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+#import "CHMovie.h"
+
+@implementation CHMovie
+
+@end
